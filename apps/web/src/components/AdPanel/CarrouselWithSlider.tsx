cc2c4869052d0a9f9df727cb106744b1998605c8
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-10-31 15:12:31 +0800

    chore: swiper with slider

---
+++ apps/web/src/components/AdPanel/CarrouselWithSlider.tsx
@@ -0,0 +1,30 @@
+import { appearAnimation } from '@pancakeswap/uikit'
+import { styled } from 'styled-components'
+import 'swiper/css'
+import 'swiper/css/effect-fade'
+import 'swiper/css/pagination'
+import { Swiper } from 'swiper/react'
+
+export const StyledSwiper = styled(Swiper)`
+  position: relative;
+  overflow: visible;
+  opacity: 0;
+  animation: ${appearAnimation} 0.3s ease-in-out 0.7s forwards;
+  .swiper-pagination {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    justify-content: center;
+    width: 108px;
+    bottom: 16px;
+  }
+  .swiper-pagination-bullet {
+    background-color: white;
+    flex-basis: 108px;
+    margin: 0 !important;
+    border-radius: 0px;
+    gap: 4px;
+    border-radius: 4px;
+  }
+`
