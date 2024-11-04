961fe6da81eb39dd1e2e8142d894a0181e1e8b0d
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-11-04 15:37:11 +0800

    chore: add animation for slider

--- apps/web/src/components/AdPanel/CarrouselWithSlider.tsx
+++ apps/web/src/components/AdPanel/CarrouselWithSlider.tsx
@@ -1,9 +1,14 @@
 import { appearAnimation } from '@pancakeswap/uikit'
-import { styled } from 'styled-components'
+import { keyframes, styled } from 'styled-components'
 import 'swiper/css'
 import 'swiper/css/effect-fade'
 import 'swiper/css/pagination'
 import { Swiper } from 'swiper/react'
+
+const progress = keyframes`
+  from {transform: scaleX(0.05);}
+  to { transform: scaleX(1);}
+`
 
 export const StyledSwiper = styled(Swiper)`
   position: absolute;
@@ -24,10 +29,27 @@ export const StyledSwiper = styled(Swiper)`
     gap: 4px;
   }
   .swiper-pagination-bullet {
-    background-color: ${({ theme }) => theme.colors.secondary};
+    position: relative;
+    background-color: ${({ theme }) => theme.colors.inputSecondary};
     margin: 0 !important;
     flex-grow: 1;
     border-radius: 4px;
     height: 4px;
+    overflow: hidden;
+    opacity: 1;
+    &.swiper-pagination-bullet-active {
+      &::before {
+        content: '';
+        border-radius: 4px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: ${({ theme }) => theme.colors.secondary};
+        animation: ${progress} 5s linear forwards;
+        transform-origin: left center;
+      }
+    }
   }
 `
