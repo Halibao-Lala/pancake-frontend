d3885e55ff1ba4699ca639b3ebf70b1e0814ee56
wenty22 <wenty.l@nodereal.io>
2024-11-27 15:45:48 +0800

    feat: Update chain & token order

--- packages/pcs-bridge/src/hooks/useTransferConfig.ts
+++ packages/pcs-bridge/src/hooks/useTransferConfig.ts
@@ -1,7 +1,8 @@
-import { ITransferConfig } from '@bnb-chain/canonical-bridge-widget'
+import { ICBridgeTransferConfig, IDeBridgeTransferConfig, ITransferConfig } from '@bnb-chain/canonical-bridge-widget'
 import { useEffect, useState } from 'react'
 
-import cBridgeConfig from '../token-config/mainnet/cBridge/config.json'
+import axios from 'axios'
+import { env } from '../configs/env'
 import layerZeroConfig from '../token-config/mainnet/layerZero/config.json'
 import mesonConfig from '../token-config/mainnet/meson/config.json'
 import stargateConfig from '../token-config/mainnet/stargate/config.json'
@@ -11,13 +12,13 @@ export function useTransferConfig() {
 
   useEffect(() => {
     const initConfig = async () => {
-      // const [cBridgeRes, deBridgeRes] = await Promise.all([
-      //   axios.get<{ data: ICBridgeTransferConfig }>(`${env.SERVER_ENDPOINT}/api/bridge/cbridge`),
-      //   axios.get<{ data: IDeBridgeTransferConfig }>(`${env.SERVER_ENDPOINT}/api/bridge/debridge`),
-      // ])
+      const [cBridgeRes, deBridgeRes] = await Promise.all([
+        axios.get<{ data: ICBridgeTransferConfig }>(`${env.SERVER_ENDPOINT}/api/bridge/cbridge`),
+        axios.get<{ data: IDeBridgeTransferConfig }>(`${env.SERVER_ENDPOINT}/api/bridge/debridge`),
+      ])
 
-      // const cBridgeConfig = cBridgeRes.data.data
-      // const deBridgeConfig = handleDeBridgeConfig(deBridgeRes.data.data)
+      const cBridgeConfig = cBridgeRes.data.data
+      const deBridgeConfig = handleDeBridgeConfig(deBridgeRes.data.data)
 
       const tokenConfig: ITransferConfig = {
         defaultSelectedInfo: {
@@ -27,8 +28,56 @@ export function useTransferConfig() {
           amount: '',
         },
         order: {
-          chains: [56, 204, 1, 42161, 8453, 728126428, 7565164, 137, 43114, 81457, 59144, 10],
-          tokens: [],
+          chains: [56, 1, 137, 324, 42161, 59144, 8453, 204],
+          tokens: [
+            'CAKE',
+            'USDC',
+            'USDT',
+            'FDUSD',
+            'USDC.e',
+            'ETH',
+            'wBETH',
+            'wstETH',
+            'weETH',
+            'UNI',
+            'AAVE',
+            'LDO',
+            'LINK',
+            'BTC',
+            'BTCB',
+            'WBTC',
+            'sUSDe',
+            'DOGE',
+            'ADA',
+            'DAI',
+            'XRP',
+            'PEPE',
+            'ELON',
+            'FLOKI',
+            'MAGA',
+            'BabyDoge',
+            'BABYGROK',
+            'PLANET',
+            'OMNI',
+            'AGI',
+            'FET',
+            'AIOZ',
+            'AI',
+            'NFP',
+            'CGPT',
+            'PHB',
+            'ZIG',
+            'NUM',
+            'GHX',
+            'PENDLE',
+            'RDNT',
+            'ROSE',
+            'HOOK',
+            'MASK',
+            'EDU',
+            'MBOX',
+            'BNX',
+          ],
         },
         displayTokenSymbols: {
           10: {
@@ -39,6 +88,10 @@ export function useTransferConfig() {
           },
           137: {
             '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174': 'USDC.e',
+          },
+          324: {
+            '0x1d17CBcF0D6D143135aE902365D2E5e2A16538D4': 'USDC',
+            '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4': 'USDC.e',
           },
           42161: {
             '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8': 'USDC.e',
@@ -60,15 +113,33 @@ export function useTransferConfig() {
           },
           bridgedTokenGroups: [],
         },
-        // deBridge: {
-        //   config: deBridgeConfig,
-        //   exclude: {
-        //     chains: [],
-        //     tokens: {
-        //     },
-        //   },
-        //   bridgedTokenGroups: [],
-        // },
+        deBridge: {
+          config: deBridgeConfig,
+          exclude: {
+            chains: [],
+            tokens: {
+              1: ['cUSDCv3'],
+              56: ['0x67d66e8ec1fd25d98b3ccd3b19b7dc4b4b7fc493', '0x0000000000000000000000000000000000000000'],
+              137: ['cUSDCv3'],
+              42161: ['cUSDCv3'],
+              43114: ['BNB'],
+            },
+          },
+          bridgedTokenGroups: [
+            ['USDT', 'USDT.e'],
+            ['USDC', 'USDC.e'],
+            ['WETH', 'WETH.e'],
+            ['DAI', 'DAI.e'],
+            ['WBTC', 'WBTC.e'],
+            ['LINK', 'LINK.e'],
+            ['AAVE', 'AAVE.e'],
+            ['WOO', 'WOO.e'],
+            ['BUSD', 'BUSD.e'],
+            ['ALPHA', 'ALPHA.e'],
+            ['SUSHI', 'SUSHI.e'],
+            ['SWAP', 'SWAP.e'],
+          ],
+        },
         stargate: {
           config: stargateConfig,
           exclude: {
@@ -108,61 +179,62 @@ export function useTransferConfig() {
   return transferConfig
 }
 
-// function handleDeBridgeConfig(rawConfig: IDeBridgeTransferConfig) {
-//   const deBridgeConfig = {
-//     ...rawConfig,
-//   }
+function handleDeBridgeConfig(rawConfig: IDeBridgeTransferConfig) {
+  const deBridgeConfig = {
+    ...rawConfig,
+  }
 
-//   const extraConfigs: Record<number, any[]> = {
-//     1: [
-//       {
-//         action: 'replace',
-//         target: '0xebd9d99a3982d547c5bb4db7e3b1f9f14b67eb83',
-//         data: {
-//           address: '0x2dfF88A56767223A5529eA5960Da7A3F5f766406',
-//           symbol: 'ID',
-//           decimals: 18,
-//           name: 'SPACE ID',
-//           logoURI: '',
-//           eip2612: false,
-//           tags: ['tokens'],
-//         },
-//       },
-//       {
-//         action: 'append',
-//         data: {
-//           address: '0x152649eA73beAb28c5b49B26eb48f7EAD6d4c898',
-//           symbol: 'Cake',
-//           decimals: 18,
-//           name: 'PancakeSwap Token',
-//           logoURI: '',
-//           eip2612: false,
-//           tags: ['tokens'],
-//         },
-//       },
-//     ],
-//   }
+  const extraConfigs: Record<number, any[]> = {
+    1: [
+      {
+        action: 'replace',
+        target: '0xebd9d99a3982d547c5bb4db7e3b1f9f14b67eb83',
+        data: {
+          address: '0x2dfF88A56767223A5529eA5960Da7A3F5f766406',
+          symbol: 'ID',
+          decimals: 18,
+          name: 'SPACE ID',
+          logoURI: '',
+          eip2612: false,
+          tags: ['tokens'],
+        },
+      },
+      {
+        action: 'append',
+        data: {
+          address: '0x152649eA73beAb28c5b49B26eb48f7EAD6d4c898',
+          symbol: 'Cake',
+          decimals: 18,
+          name: 'PancakeSwap Token',
+          logoURI: '',
+          eip2612: false,
+          tags: ['tokens'],
+        },
+      },
+    ],
+  }
 
-//   Object.entries(deBridgeConfig.tokens).forEach(([key, value]) => {
-//     const chainId = Number(key)
-//     const extraConfig = extraConfigs[chainId]
+  Object.entries(deBridgeConfig.tokens).forEach(([key, value]) => {
+    const chainId = Number(key)
+    const extraConfig = extraConfigs[chainId]
 
-//     if (extraConfig) {
-//       extraConfig.forEach((network) => {
-//         const { action, target, data } = network
-//         if (!value[data.address]) {
-//           if (action === 'replace') {
-//             const index = value.findIndex((item) => item.address === target)
-//             if (index > -1) {
-//               value[index] = data
-//             }
-//           } else if (action === 'append') {
-//             ;(value as any).push(data)
-//           }
-//         }
-//       })
-//     }
-//   })
+    if (extraConfig) {
+      extraConfig.forEach((network) => {
+        const { action, target, data } = network
+        if (!value[data.address]) {
+          if (action === 'replace') {
+            const index = value.findIndex((item) => item.address === target)
+            if (index > -1) {
+              // eslint-disable-next-line no-param-reassign
+              value[index] = data
+            }
+          } else if (action === 'append') {
+            ;(value as any).push(data)
+          }
+        }
+      })
+    }
+  })
 
-//   return deBridgeConfig
-// }
+  return deBridgeConfig
+}
